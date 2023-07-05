b14c533bfd626ab008a91bd7a225c53b576072b3
Tai-Yu Chen <86171033+prince-of-tennis@users.noreply.github.com>
2023-07-05 14:22:56 -0700

    Update upload-camp-images.bat

--- upload-camp-images.bat
+++ upload-camp-images.bat
@@ -1,4 +1,7 @@
+rem set the folder or drive with contents you want completedly deleted.
 set folder="W:"
+
+
 cd /d %folder%
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 cd "W:
